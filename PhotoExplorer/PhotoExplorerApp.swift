c48f6b0b88a02f8c6b1476d79a3da6ee00417636
Marcel Mravec <marcel.mravec@cleverlance.com>
2023-10-02 18:32:20 +0200

    Initial Commit

---
+++ PhotoExplorer/PhotoExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoExplorerApp.swift
+//  PhotoExplorer
+//
+//  Created by Marcel Mravec on 02.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct PhotoExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
